0247906362028156cdba5635da45041ee717b42d
Jett Crowson <jettcrowson@gmail.com>
2025-11-29 19:47:00 -0700

    Add flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "jett's nixos-config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    {
+      nixosConfigurations.mothership = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/mothership/configuration.nix
+        ];
+      };
+    };
+}
